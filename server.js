895b7069e4bfdfaf328dfe92db63fed450d7824c
akmal <akmal@brighter.com>
2014-12-01 12:10:31 -0800

    added authentication - initial setup

--- server.js
+++ server.js
@@ -4,8 +4,47 @@ var app = express();
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
-// logger to log events
+// log events
 var logger = require('nlogger').logger(module);
+
+// authentication module
+var passport = require('passport');
+var passportLocal = require('passport-local');
+var cookieParser = require('cookie-parser');
+var expressSession = require('express-session');
+
+app.use(cookieParser());
+app.use(expressSession({
+  secret : 'mySecretKey',
+  resave: false,
+  saveUninitialized: true
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(function(user, done){
+  logger.debug('serializing: ', user.id);
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done){
+  // query database to fetch the user based on id
+  logger.debug('de-serializing: ', id);
+  var user = getUser(users, id);
+  done(null, {id: id, name: user.name, email: user.email});
+});
+
+passport.use(new passportLocal.Strategy(function (username, password, done){
+  logger.info("username: ", username, "password", password);
+  var user = getUser(users, username);
+  if(user) { //authentication successful
+    done(null, user);
+  } else { // authentication failed
+  	done(new Error('failed to authenticate'));
+  } 
+}));
+
 
 //temp data
 var users = [{id:'akmal', password:'test',name: 'AM', email : 'a@m.com'},
@@ -16,27 +55,28 @@ var posts = [{author: 'akmal', body:'my first post', date: Date.now()},
 
 // Route implementations
 // get all users, supports login operation
-app.get('/api/users/', function getAllUsers (req,res) {
-  var operation = req.query.operation;
-  if(operation && operation == 'login') {
-    logger.info("attempting login . username: ",
-        req.query.username, " password: ", req.query.password);
-    var user = getUser(users, req.query.username);
-    if( user && user.password  == req.query.password) {
-      logger.info('login successful. User : ', user);
-      return res.send({users : [user]});
-    } else {
-      logger.error('login failed for username: ', req.query.username);
-      res.status(404).end();
+app.get('/api/users/', passport.authenticate('local'), function getAllUsers (req,res) {
+  logger.debug("in getAllUSers");
+  if(req.isAuthenticated()) {
+    if(req.query.operation == 'login'){
+      return res.send({users : [req.user]});
+    } else { //return all users
+   	  logger.debug("retrieving all users");
+      return res.send({users : users});
     }
-  } else { // return all the users
-    logger.info("retrieved users: ", users);
-    return res.send({users : users});
+  } else {
+  	//redirect to login
+  	res.status(401).end();
   }
 });
 
 //get user by id
-app.get('/api/users/:userId', function getUserById (req, res) {
+app.get('/api/users/:userId', passport.authenticate('local'),function getUserById (req, res) {
+  if(req.isAuthenticated()) {
+  	logger.info('getUserById user is authentciated');
+  } else {
+  	logger.info("user not authentciated")
+  }
   logger.info("attempting to retrieve user with userId: ", req.params.userId);
   var user = getUser(users, req.params.userId)
   if (user) {
