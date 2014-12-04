cb9d37bf31a1468aec3f13baa3fc7a0d0141c290
akmal <akmal@brighter.com>
2014-12-03 23:48:16 -0800

    format indentation

--- server.js
+++ server.js
@@ -62,16 +62,14 @@ var UserModel;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function callback () {
   console.log("Connected to telegramDb");
-
   var userSchema = mongoose.Schema(
-  	{
-  		id : String,
-  		password: String,
-  		name: String,
-  		email: String
-  	});
-
-   UserModel = mongoose.model('User', userSchema);
+    {
+      id : String,
+      password: String,
+      name: String,
+      email: String
+    });
+  UserModel = mongoose.model('User', userSchema);
 });
 
 
